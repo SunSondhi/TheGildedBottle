a4a12dbe1859b3b199dfb42b0658f25e84aeba81
Aston-JoshN <116265079+Aston-JoshN@users.noreply.github.com>
2023-02-14 15:38:44 +0000

    Fixed initial issue, working on the panels from here on out

--- TheGildedBottleApp/src/main/java/org/theGildedBottle/ContentManager.java
+++ TheGildedBottleApp/src/main/java/org/theGildedBottle/ContentManager.java
@@ -1,5 +1,47 @@
 package org.theGildedBottle;
+import org.jetbrains.annotations.NotNull;
 
-public class ContentManager {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class ContentManager implements ActionListener { //using the content manager to manage the javax.swing Card Layout system in a way that works for our use case
+    JPanel contentPanel;
+    JPanel currentPanel;
+    CardLayout cL;
+    JFrame window;
+
+    public ContentManager (@NotNull JFrame window) {
+        cL = new CardLayout();
+        this.window = window;
+        contentPanel = new JPanel();
+        contentPanel.setLayout(cL);
+        currentPanel = new LoginPanel(this);
+        this.window.add(contentPanel);
+        contentPanel.add(currentPanel);
+        window.setVisible(true);
+    }
+    public ContentManager(@NotNull JFrame window, JPanel startPanel) {
+        cL = new CardLayout();
+        this.window = window;
+        contentPanel = new JPanel();
+        contentPanel.setLayout(cL);
+        currentPanel = startPanel;
+        window.add(contentPanel);
+        contentPanel.add(currentPanel);
+    }
+
+    public void ChangeScene(@NotNull JPanel nextPanel) {
+        contentPanel.remove(currentPanel);
+        currentPanel = nextPanel;
+        contentPanel.add(currentPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        contentPanel.remove(currentPanel);
+        contentPanel.add(new ProductPanel());
+        contentPanel.revalidate();
+    }
 }
