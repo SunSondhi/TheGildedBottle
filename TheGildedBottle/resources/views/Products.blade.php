ff596c455b8744869e45b8207293f97f32513871
SunSondhi <114810896+SunSondhi@users.noreply.github.com>
2023-02-21 23:11:29 +0000

    fixed the issue that was arising from the products page, really silly mistakes...

--- TheGildedBottle/resources/views/Products.blade.php
+++ TheGildedBottle/resources/views/Products.blade.php
@@ -1,104 +1,65 @@
-@section('title','| Products')
+@section('title', '| Products')
 @include('layouts/head')
 @include('layouts/nav')
 
-
-
-
-
-<?php
-
-use Illuminate\Support\Facades\Auth;
-
-if (Auth::check() && Auth::user()->role == '1') {
-    print '<p>you can see THIS because you are an admin</p>'; // this is when user is an admin
-} elseif (Auth::check() && Auth::user()->role == '2') {
-    print '<p>you are seeing this beacause you are an employee</p>'; //this is beacuse this is an employee user
-} else {
-    print '<p>you cannot see this beacause you are not Admin or AND employee but a customer</p>'; //this is a normal user 
-}
-?>
-
-
 <div class="main-content">
+    <?php
+
+    use Illuminate\Support\Facades\Auth;
+
+    if (Auth::check() && Auth::user()->role == '1') {
+        echo '<p>You can see THIS because you are an admin</p>';
+    } elseif (Auth::check() && Auth::user()->role == '2') {
+        echo '<p>You are seeing this because you are an employee</p>';
+    } else {
+        echo '<p>You cannot see this because you are not an admin or an employee, but a customer</p>';
+    }
+    ?>
+
     <h1>List of all products</h1>
 
-
-
-
-
-    <div class="row">
-        <h3>Product Category</h3>
-        <div class="flex-container">
-            @foreach ($products as $us)
-            <div class="card2">
-                <div>
-                <a href="Product_details/{{$us->id}}"> <img src="{{ $us->image }}" alt=""></a>
-                </div>
-                <div>
-                    <h4>{{ $us->name }}</h4>
-                    <p>{{ $us->description }}</p>
-                    <p><strong>Price: </strong> £{{ $us->price }}</p>
-                    <?php
-                    if ((Auth::check() && Auth::user()->role == '1') || (Auth::check() && Auth::user()->role == '2')) {
-                    ?>
-                        <p><strong>Quantity: </strong> {{ $us->quantity }}</p><?php
-
-                                                                            } ?>
-                </div>
-            <div class="filter-btn">
-                <form action="{{ route('products.filter.category', ['productCat' => 'Rum']) }}" method="get">
-                    <button type="submit" class="btn btn-primary">Rum</button>
-                </form>
-                <form action=" {{ route('products.filter.category', ['productCat' => 'Whiskey']) }}" method="get">
-                    <button type="submit" class="btn btn-primary">Whiskey</button>
-                </form>
-                <form action="{{ route('products.filter.category', ['productCat' => 'Vodka']) }}" method="get">
-                    <button type="submit" class="btn btn-primary">Vodka</button>
-                </form>
-                <form action="{{ route('products.filter.category', ['productCat' => 'Gin']) }}" method="get">
-                    <button type="submit" class="btn btn-primary">Gin</button>
-                </form>
-                <form action="{{ route('products.filter.price', ['price' => '$minPrice - $maxPrice']) }}" method="get">
-                    <button type="submit" class="btn btn-primary">$50 - $100</button>
-                </form>
-                <form action="{{ route('products.filter.price') }}" method="GET">
-                    <label for="min_price">Minimum price:</label>
-                    <input type="number" name="min_price" id="min_price">
-
-                    <label for="max_price">Maximum price:</label>
-                    <input type="number" name="max_price" id="max_price">
-
-                    <button type="submit">Filter</button>
-                </form>
-            </div>
-        </div>
+    <h3>Product Category</h3>
+    <div class="filter-btn">
+        <form action="{{ route('products.filter.category', ['productCat' => 'Rum']) }}" method="get">
+            <button type="submit" class="btn btn-primary">Rum</button>
+        </form>
+        <form action="{{ route('products.filter.category', ['productCat' => 'Whiskey']) }}" method="get">
+            <button type="submit" class="btn btn-primary">Whiskey</button>
+        </form>
+        <form action="{{ route('products.filter.category', ['productCat' => 'Vodka']) }}" method="get">
+            <button type="submit" class="btn btn-primary">Vodka</button>
+        </form>
+        <form action="{{ route('products.filter.category', ['productCat' => 'Gin']) }}" method="get">
+            <button type="submit" class="btn btn-primary">Gin</button>
+        </form>
+        <form action="{{ route('products.filter.price') }}" method="get">
+            <label for="min_price">Minimum price:</label>
+            <input type=" number" name="min_price" id="min_price">
+            <label for="max_price">Maximum price:</label>
+            <input type="number" name="max_price" id="max_price">
+            <button type="submit">Filter</button>
+        </form>
     </div>
-
-
-
-
 
 
     <div class="row">
         <div class="col">
             <div class="flex-container">
                 @if (count($products) > 0)
-                @foreach ($products as $us)
+                @foreach ($products as $product)
                 <div class="card">
-                    <h4>{{ $us->name }}</h4>
-                    <img id="product-card-image" src="{{ $us->image }}">
-                    <p><strong>Price: </strong> £{{ $us->price }}</p>
-                    <button>click me</button>
-                </div>
-                <div>
-                    <?php
-                    if ((Auth::check() && Auth::user()->role == '1') || (Auth::check() && Auth::user()->role == '2')) {
-                    ?>
-                        <p><strong>Quantity: </strong> {{ $us->quantity }}</p>
-                    <?php
-                    }
-                    ?>
+                    <div class="card-image">
+                        <img src="{{ $product->image }}" alt="{{ $product->name }}">
+                    </div>
+                    <div class="card-info">
+                        <h4 class="card-title">{{ $product->name }}</h4>
+                        <p class="card-description">{{ $product->description }}</p>
+                        <p class="card-price"><strong>Price: </strong> £{{ $product->price }}</p>
+                        @if (Auth::check() && in_array(Auth::user()->role, ['1', '2']))
+                        <p class="card-quantity"><strong>Quantity: </strong> {{ $product->quantity }}</p>
+                        @endif
+                        <button class="card-button">Add to cart</button>
+                    </div>
                 </div>
                 @endforeach
                 @else
@@ -107,15 +68,5 @@ if (Auth::check() && Auth::user()->role == '1') {
             </div>
         </div>
     </div>
-
-
-
-
-
-
-
-
-
-
 
 </div>
