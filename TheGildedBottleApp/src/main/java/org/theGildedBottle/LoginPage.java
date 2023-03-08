3c10e6fb386e14ecc6d46f8f778b08595170276d
SunSondhi <114810896+SunSondhi@users.noreply.github.com>
2023-03-08 09:49:02 +0000

    implementing the login system using maven and JBCRYPT

--- TheGildedBottleApp/src/main/java/org/theGildedBottle/LoginPage.java
+++ TheGildedBottleApp/src/main/java/org/theGildedBottle/LoginPage.java
@@ -3,6 +3,13 @@ package org.theGildedBottle;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.*;
+import org.mindrot.jbcrypt.BCrypt;
+
+import static org.theGildedBottle.DbCon.con;
 
 public class LoginPage {
     private JTextField emailField;
@@ -11,9 +18,55 @@ public class LoginPage {
     private JLabel emailLabel;
     private JLabel passwordLabel;
     public JPanel loginPage;
+
+    public static DbCon Con = new DbCon();
+    private Statement stmt;
+
     public LoginPage(ContentManager cM) {
+
         loginPage.setBackground(ContentManager.BACKGROUND_COLOUR);
-        loginButton.setActionCommand("Login");
-        loginButton.addActionListener(cM);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String userEmail = emailField.getText();
+                String password = passwordField.getText();
+                Connection con = null;
+
+                try {
+                    con = DbCon.getConnection();
+                    String sql = "SELECT * FROM users WHERE email = ?";
+                    PreparedStatement stmt = con.prepareStatement(sql);
+                    stmt.setString(1, userEmail);
+                    ResultSet result = stmt.executeQuery();
+
+                    if (result.next()) {
+                        String storedHash = result.getString("password");
+
+                        // Check if the password matches the stored hash
+                        if (BCrypt.checkpw(password, storedHash)) {
+                            // Password is correct, log in the user
+                            System.out.println("Logged in successfully");
+                        } else {
+                            // Password is incorrect, show error message
+                            System.out.println("Incorrect password");
+                        }
+                    } else {
+                        // User not found, show error message
+                        System.out.println("User not found");
+                    }
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                } finally {
+                    try {
+                        if (con != null) {
+                            con.close(); // Close the connection
+                        }
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
