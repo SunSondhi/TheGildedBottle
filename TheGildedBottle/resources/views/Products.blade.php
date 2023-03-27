3d6d42b710a2b7836f14d6008341971b059dee9e
SunSondhi <114810896+SunSondhi@users.noreply.github.com>
2023-03-28 00:25:15 +0100

    Login system fully working

--- TheGildedBottle/resources/views/Products.blade.php
+++ TheGildedBottle/resources/views/Products.blade.php
@@ -54,48 +54,49 @@
 
                     <form method="get">
                         <h5>Search By Name</h5>
-                    <form method="get" action="{{route('products')}}">
-                        <input type="text" class="form-control" name="search_entry" id="search_entry" placeholder="Search...">
-                        <button type="submit" class="btn btn-success">search</button>
-                    </form>
+                        <form method="get" action="{{route('products')}}">
+                            <input type="text" class="form-control" name="search_entry" id="search_entry" placeholder="Search...">
+                            <button type="submit" class="btn btn-success">search</button>
+                        </form>
                 </div>
             </div>
         </div>
 
         <div class="col-md-8">
-    <div class="row" style="margin-top:2%; margin-left:-20%; margin-right:2%;">
-        @if (count($products) > 0)
-        @foreach ($products as $product)
-        <div class="col-md-4 mb-3">
-            <a href="{{url('Product_details')}}/{{($product->id)}}" style="text-decoration:none;">
-                <div class="card" style="border: 1px solid #ccc; border-radius: 10px; box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);">
-                    <img src="{{ url($product->image) }}" alt="{{ $product->name }}" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover; border-radius: 10px 10px 0 0;">
-                    <div class="card-body" style="padding: 1rem;">
-                        <h4 class="card-title" style="font-size: 1.2rem; font-weight: bold; margin-bottom: 0;">{{ $product->name }}</h4>
-                        <p class="card-text" style="font-size: 1.1rem; margin-bottom: 0;">Price: £{{ $product->price }}</p>
-                        <?php
-                        if ($product->stock >= 5) { ?>
-                            <p class="card-text" style="font-size: 0.9rem; margin-bottom: 0;">Stock: {{ $product->stock }}</p>
-                        <?php
-                        } elseif ($product->stock < 5 && $product->stock > 0) { ?>
-                            <div class="alert alert-warning" role="alert" style="padding: 0.5rem 1rem; font-size: 0.9rem; margin-bottom: 0;">
-                                <p class="card-text" style="margin-bottom: 0;">Stock: {{ $product->stock }} </p>
+            <div class="row" style="margin-top:2%; margin-left:-20%; margin-right:2%;">
+                @if (count($products) > 0)
+                @foreach ($products as $product)
+                <div class="col-md-4 mb-3">
+                    <a href="{{url('Product_details')}}/{{($product->id)}}" style="text-decoration:none;color:black;">
+                        <div class="card" style="border: 1px solid #ccc; border-radius: 10px; box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);">
+                            <img src="{{ url($product->image) }}" alt="{{ $product->name }}" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover; border-radius: 10px 10px 0 0;">
+                            <div class="card-body" style="padding: 1rem;">
+                                <h4 class="card-title" style="font-size: 1.2rem; font-weight: bold; margin-bottom: 0;">{{ $product->name }}</h4>
+
+                                <?php
+                                if ($product->stock >= 5) { ?>
+                                    <p class="card-text" style="font-size: 0.9rem; margin-bottom: 0;">Stock: {{ $product->stock }}</p>
+                                <?php
+                                } elseif ($product->stock < 5 && $product->stock > 0) { ?>
+                                    <div class="alert alert-warning" role="alert" style="padding: 0.5rem 1rem; font-size: 0.9rem; margin-bottom: 0;">
+                                        <p class="card-text" style="margin-bottom: 0;">Stock: {{ $product->stock }} </p>
+                                    </div>
+                                <?php
+                                } else { ?>
+                                    <div class="alert alert-danger" role="alert" style="padding: 0.5rem 1rem; font-size: 0.9rem; margin-bottom: 0;">
+                                        <p class="card-text" style="margin-bottom: 0;">Out of stock</p>
+                                    </div>
+                                <?php } ?>
+                                <p class="card-text" style="font-size: 1.5rem; margin-bottom: 0;">Price: £{{ $product->price }}</p>
                             </div>
-                        <?php
-                        } else { ?>
-                            <div class="alert alert-danger" role="alert" style="padding: 0.5rem 1rem; font-size: 0.9rem; margin-bottom: 0;">
-                                <p class="card-text" style="margin-bottom: 0;">Out of stock</p>
-                            </div>
-                        <?php } ?>
-                    </div>
+                        </div>
+                    </a>
                 </div>
-            </a>
+                @endforeach
+                @else
+                <div class="col-md-12">
+                    <p>No products found</p>
+                </div>
+                @endif
+            </div>
         </div>
-        @endforeach
-        @else
-        <div class="col-md-12">
-            <p>No products found</p>
-        </div>
-        @endif
-    </div>
-</div>
