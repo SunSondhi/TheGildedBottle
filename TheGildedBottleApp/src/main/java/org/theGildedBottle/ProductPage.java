ca92d00be740b2a6f888297e707b11c3adfc25d1
Aston-JoshN <116265079+Josh-XENOCRACY@users.noreply.github.com>
2023-03-30 23:10:37 +0100

    Revert "Executable jar file ready for distribution."
    This reverts commit 5bd7787ebd4efac9be3055f4a74b79e9b674e2ff.

--- TheGildedBottleApp/src/main/java/org/theGildedBottle/ProductPage.java
+++ TheGildedBottleApp/src/main/java/org/theGildedBottle/ProductPage.java
@@ -1,11 +1,6 @@
 package org.theGildedBottle;
 
 import javax.swing.*;
-<<<<<<< Updated upstream
-=======
-import javax.swing.text.html.StyleSheet;
-import java.awt.*;
->>>>>>> Stashed changes
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -43,7 +38,7 @@ public class ProductPage {
     private JTextField stockField;
 
     private Statement stmt;
-    public DbCon Con = new DbCon();
+    public DbCon Con =  new DbCon();
 
     public ProductPage(ContentManager cM) {
         try {
@@ -52,42 +47,41 @@ public class ProductPage {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        addProductButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String name = nameField.getText();
-                double price = Double.parseDouble(priceField.getText());
-                int quantity = Integer.parseInt(quantityField.getText());
-                float percentage = Integer.parseInt(percentageField.getText());
-                String description = descriptionArea.getText();
-                String productCAT = categoryField.getText();
-                String type = typeField.getText();
-                String flavour = flavourField.getText();
-                String image = imageField.getText();
-                int stock = Integer.parseInt(stockField.getText());
-                try {
-                    String sql = "INSERT INTO products (name, price, quantity, percentage, description, productCat, type, flavour, image,stock) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
-                    PreparedStatement statement = con.prepareStatement(sql);
-                    statement.setString(1, name);
-                    statement.setDouble(2, price);
-                    statement.setInt(3, quantity);
-                    statement.setInt(4, (int) percentage);
-                    statement.setString(5, description);
-                    statement.setString(6, productCAT);
-                    statement.setString(7, type);
-                    statement.setString(8, flavour);
-                    statement.setString(9, image);
-                    statement.setString(10, String.valueOf(stock));
-                    statement.executeUpdate();
+    addProductButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String name = nameField.getText();
+            double price = Double.parseDouble(priceField.getText());
+            int quantity = Integer.parseInt(quantityField.getText());
+            float percentage = Integer.parseInt(percentageField.getText());
+            String description = descriptionArea.getText();
+            String productCAT = categoryField.getText();
+            String type = typeField.getText();
+            String flavour = flavourField.getText();
+            String image = imageField.getText();
+            int stock = Integer.parseInt(stockField.getText());
+            try {
+                String sql = "INSERT INTO products (name, price, quantity, percentage, description, productCat, type, flavour, image,stock) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
+                PreparedStatement statement = con.prepareStatement(sql);
+                statement.setString(1, name);
+                statement.setDouble(2, price);
+                statement.setInt(3, quantity);
+                statement.setInt(4, (int) percentage);
+                statement.setString(5, description);
+                statement.setString(6, productCAT);
+                statement.setString(7, type);
+                statement.setString(8, flavour);
+                statement.setString(9, image);
+                statement.setString(10, String.valueOf(stock));
+                statement.executeUpdate();
 
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
             }
-        });
+        }
+    });
 
 
-<<<<<<< Updated upstream
 
 
     this.clearButton.addActionListener(new ActionListener() {
@@ -106,122 +100,9 @@ public class ProductPage {
     });
     this.logoutButton.addActionListener(cM);
     this.logoutButton.setActionCommand("Logout");
-=======
-        this.clearButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                nameField.setText("");
-                priceField.setText("");
-                quantityField.setText("");
-                percentageField.setText("");
-                categoryField.setText("");
-                typeField.setText("");
-                flavourField.setText("");
-                imageField.setText("");
-                descriptionArea.setText("");
-            }
-        });
->>>>>>> Stashed changes
 
         this.gotToHomepageButton.addActionListener(cM);
         this.gotToHomepageButton.setActionCommand("Homepage");
 
-    }
-
-    {
-// GUI initializer generated by IntelliJ IDEA GUI Designer
-// >>> IMPORTANT!! <<<
-// DO NOT EDIT OR ADD ANY CODE HERE!
-        $$$setupUI$$$();
-    }
-
-    /**
-     * Method generated by IntelliJ IDEA GUI Designer
-     * >>> IMPORTANT!! <<<
-     * DO NOT edit this method OR call it in your code!
-     *
-     * @noinspection ALL
-     */
-    private void $$$setupUI$$$() {
-        p1 = new JPanel();
-        p1.setLayout(new BorderLayout(0, 0));
-        productsPanel = new JPanel();
-        productsPanel.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(13, 5, new Insets(0, 0, 0, 0), -1, -1));
-        productsPanel.setForeground(new Color(-9277845));
-        p1.add(productsPanel, BorderLayout.CENTER);
-        nameField = new JTextField();
-        productsPanel.add(nameField, new com.intellij.uiDesigner.core.GridConstraints(0, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(193, 30), null, 0, false));
-        final com.intellij.uiDesigner.core.Spacer spacer1 = new com.intellij.uiDesigner.core.Spacer();
-        productsPanel.add(spacer1, new com.intellij.uiDesigner.core.GridConstraints(0, 4, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
-        priceField = new JTextField();
-        productsPanel.add(priceField, new com.intellij.uiDesigner.core.GridConstraints(1, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(193, 30), null, 0, false));
-        final com.intellij.uiDesigner.core.Spacer spacer2 = new com.intellij.uiDesigner.core.Spacer();
-        productsPanel.add(spacer2, new com.intellij.uiDesigner.core.GridConstraints(0, 3, 12, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_VERTICAL, 1, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-        final com.intellij.uiDesigner.core.Spacer spacer3 = new com.intellij.uiDesigner.core.Spacer();
-        productsPanel.add(spacer3, new com.intellij.uiDesigner.core.GridConstraints(11, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_VERTICAL, 1, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(193, 14), null, 0, false));
-        priceLabel = new JLabel();
-        priceLabel.setText("Price ");
-        productsPanel.add(priceLabel, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(112, 16), null, 0, false));
-        quantityField = new JTextField();
-        productsPanel.add(quantityField, new com.intellij.uiDesigner.core.GridConstraints(2, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(193, 30), null, 0, false));
-        quantityLabel = new JLabel();
-        quantityLabel.setText("Quantity");
-        productsPanel.add(quantityLabel, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(112, 16), null, 0, false));
-        percentageField = new JTextField();
-        productsPanel.add(percentageField, new com.intellij.uiDesigner.core.GridConstraints(3, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(193, 30), null, 0, false));
-        abvLabel = new JLabel();
-        abvLabel.setText("Percentage");
-        productsPanel.add(abvLabel, new com.intellij.uiDesigner.core.GridConstraints(3, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(112, 16), null, 0, false));
-        categoryField = new JTextField();
-        productsPanel.add(categoryField, new com.intellij.uiDesigner.core.GridConstraints(4, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(193, 30), null, 0, false));
-        categoryLabel = new JLabel();
-        categoryLabel.setText("Category ");
-        productsPanel.add(categoryLabel, new com.intellij.uiDesigner.core.GridConstraints(4, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(112, 16), null, 0, false));
-        typeField = new JTextField();
-        productsPanel.add(typeField, new com.intellij.uiDesigner.core.GridConstraints(5, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(193, 30), null, 0, false));
-        typeLabel = new JLabel();
-        typeLabel.setText("Type ");
-        productsPanel.add(typeLabel, new com.intellij.uiDesigner.core.GridConstraints(5, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(112, 16), null, 0, false));
-        flavourField = new JTextField();
-        productsPanel.add(flavourField, new com.intellij.uiDesigner.core.GridConstraints(6, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(193, 30), null, 0, false));
-        flavourLabel = new JLabel();
-        flavourLabel.setText("Flavour notes");
-        productsPanel.add(flavourLabel, new com.intellij.uiDesigner.core.GridConstraints(6, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(112, 16), null, 0, false));
-        imageField = new JTextField();
-        productsPanel.add(imageField, new com.intellij.uiDesigner.core.GridConstraints(7, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(193, 30), null, 0, false));
-        imageLabel = new JLabel();
-        imageLabel.setText("Image ");
-        productsPanel.add(imageLabel, new com.intellij.uiDesigner.core.GridConstraints(7, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(112, 16), null, 0, false));
-        addProductButton = new JButton();
-        addProductButton.setText("Add product");
-        productsPanel.add(addProductButton, new com.intellij.uiDesigner.core.GridConstraints(1, 4, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        clearButton = new JButton();
-        clearButton.setText("Clear");
-        productsPanel.add(clearButton, new com.intellij.uiDesigner.core.GridConstraints(2, 4, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        descriptionArea = new JTextArea();
-        productsPanel.add(descriptionArea, new com.intellij.uiDesigner.core.GridConstraints(8, 2, 2, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
-        descriptionLabel = new JLabel();
-        descriptionLabel.setText("Description");
-        productsPanel.add(descriptionLabel, new com.intellij.uiDesigner.core.GridConstraints(8, 1, 2, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_NORTHWEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(112, 16), null, 0, false));
-        nameLabel = new JLabel();
-        nameLabel.setText("Name ");
-        productsPanel.add(nameLabel, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(112, 16), null, 0, false));
-        final com.intellij.uiDesigner.core.Spacer spacer4 = new com.intellij.uiDesigner.core.Spacer();
-        productsPanel.add(spacer4, new com.intellij.uiDesigner.core.GridConstraints(11, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, 1, null, new Dimension(85, 11), null, 0, false));
-        gotToHomepageButton = new JButton();
-        gotToHomepageButton.setText(" Back");
-        productsPanel.add(gotToHomepageButton, new com.intellij.uiDesigner.core.GridConstraints(12, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final JLabel label1 = new JLabel();
-        label1.setText("Stock");
-        productsPanel.add(label1, new com.intellij.uiDesigner.core.GridConstraints(10, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        stockField = new JTextField();
-        productsPanel.add(stockField, new com.intellij.uiDesigner.core.GridConstraints(10, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-    }
-
-    /**
-     * @noinspection ALL
-     */
-    public JComponent $$$getRootComponent$$$() {
-        return p1;
     }
 }
